c0f52bd3e15a557c85c1cf97d81ebdb758764023
Andres <medinac228@gmail.com>
2025-04-27 23:17:42 -0500

    Agregar archivos de proyecto.

---
+++ DesarrolloWeb2Front/WebForms/HobbiesFront.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DesarrolloWeb2Front.WebForms
+{
+    public partial class HobbiesFront : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            HobbiesWS.HobbieWS hobbieWS = new HobbiesWS.HobbieWS();
+            GridView1.DataSource = hobbieWS.HobbiesGetAll();
+            GridView1.DataBind();
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
